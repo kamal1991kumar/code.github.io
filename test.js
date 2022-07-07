930bf40b8a97a01266cd9705f35b6ce95817632c
kamal1991kumar <kamal1991kumar@gmail.com>
2022-07-07 10:59:38 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,52 @@
+//  var s = 3;
+//  var x = 5;
+
+//  console.log(s+x);
+  // x=2;
+console.log(10**2);
+let x = 'mar'
+switch(x){
+case 'jan':
+case 'mar':
+case 'may':
+case 'july':
+case 'aug':
+case 'oct':
+case 'dec':
+
+console.log('31')
+break;
+case 'apr':
+case 'jun':
+case 'sep':
+case 'nov':
+  console.log('30') 
+  break;
+}
+let f = '5'
+var output = '';
+switch(f){
+case '1':
+  output = 'monday'
+case '2':
+  output = 'monday'
+ case '3':
+  case '4':
+    case '5':
+    case '6':
+    case '7':
+console.log()
+
+}
+
+function get(m) {
+  switch(m){
+    case 1: return 'monday'
+    case 2: return 'test'
+    case 3: return 'web'
+    case 4: return 'fr'
+
+  }
+}
+
+console.log(get(1));
